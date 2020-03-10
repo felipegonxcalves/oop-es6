4a38931b48e63622609026364e0f2d328dba487a
felipeconxceicao <felipe.conceicao@infocast.com.br>
2020-03-10 17:58:32 -0300

    Atualizando master

--- Person.js
+++ Person.js
@@ -4,4 +4,16 @@ class Person {
         this.age = 25;
         this.height = 1.84;
     }
+
+    // sayHello() {
+    //     console.log(`Olá ${this.name}, como está você ?`);
+    // }
+
+    static  sayHello(name){
+        console.log(`Olá ${name}, tudo bem com você ??`);
+    }
 }
+
+let felipe = new Person();
+// felipe.sayHello();
+Person.sayHello("Felipêta");
